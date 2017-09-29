e6c85c0cbb1b8ed7181c33d6a4c4f26662a51c04
HustWolfzzb <1184827350@qq.com>
2017-09-30 00:49:46 +0800

    Demo1-main.c

--- main.c
+++ main.c
@@ -5,18 +5,48 @@
 
 void Sender_Task(void *pvParameters)
 {
-
+	int32_t Send_Num = 1;  
+    for( ;; )  
+    {  
+        vTaskDelay( 2/portTICK_RATE_MS );  
+        /* 向队列中填充内容 */  
+        xQueueSend( MyQueue, ( void* )&Send_Num, 0 );  
+        Send_Num++;  
 }
+
 void Receiver_Task(void *pvParameters)
 {
-	int 
+	int32_t  Received_Num = 0;  
+    for( ;; )  
+    {  
+        /* 从队列中获取内容 */  
+        if( xQueueReceive( MyQueue, &Received_Num, 1000/portTICK_RATE_MS ) == pdPASS)  
+        {  
+            printf("Received_Num:%d\r\n",Received_Num);  
+        }  
+    }  
 }
 
+void Monitor_Task(void *pvParameters)
+{
 
+}
 
 int main()
 {
-	xQueueHandle xQueueCreate(510,4);
-	
-	return 0;
+    /* 初始化硬件平台 */  
+    prvSetupHardware();  
+     
+    /* 建立队列 */  
+    MyQueue = xQueueCreate( 510 , sizeof( int32_t ) );  
+    /* 建立任务 */  
+    xTaskCreate( Sender_Task, ( signed portCHAR * ) "Sender_Task", configMINIMAL_STACK_SIZE,  
+                            NULL, tskIDLE_PRIORITY+3, NULL );  
+    xTaskCreate( Receiver_Task, ( signed portCHAR * ) "Receiver_Task", configMINIMAL_STACK_SIZE,  
+                            NULL, tskIDLE_PRIORITY+4, NULL );  
+    /* 启动OS */  
+
+    vTaskStartScheduler();  
+     
+    return 0; 
 }
