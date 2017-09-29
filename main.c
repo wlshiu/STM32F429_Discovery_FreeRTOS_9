04ef3f6236d3c8abef84fee6f898d8a88cb3c8d9
HustWolfzzb <1184827350@qq.com>
2017-09-29 23:04:35 +0800

    main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <queue.h>
+#include <task.h>
+
+
+
+void Sender_Task(void *pvParameters)
+{
+
+}
+void Receiver_Task(void *pvParameters)
+{
+	int 
+}
+
+
+
+int main()
+{
+	xQueueHandle xQueueCreate(510,4);
+	
+	return 0;
+}
